28e4736339cad9f98ce587d3142e5784ab0f5920
Mohamed Moussa <mohamed.moussa@mail.utoronto.ca>
2020-08-24 15:21:40 -0400

    Setup port for heroku

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function () {
-  console.log("YelpCamp Server");
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("YelpCamp Server Started");
 });
